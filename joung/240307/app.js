a5f5a85ca29963e8e19fe6f916e79401aacef9ed
JOUNG <116415851+totoback@users.noreply.github.com>
2024-03-08 11:27:54 +0900

    핏쨔

--- joung/240307/app.js
+++ joung/240307/app.js
@@ -4,7 +4,7 @@ const menu = [
       title: "buttermilk pancakes",
       category: "breakfast",
       price: 15.99,
-      img: "./1.jpg",
+      img: "./img/1.jpg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
@@ -12,7 +12,7 @@ const menu = [
       title: "diner double",
       category: "lunch",
       price: 13.99,
-      img: "./2.jpg",
+      img: "./img/2.jpg",
       desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
@@ -20,7 +20,7 @@ const menu = [
       title: "godzilla milkshake",
       category: "shakes",
       price: 6.99,
-      img: "./3.jpg",
+      img: "./img/3.jpg",
       desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
@@ -28,7 +28,7 @@ const menu = [
       title: "country delight",
       category: "breakfast",
       price: 20.99,
-      img: "./1.jpg",
+      img: "./img/1.jpg",
       desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
@@ -36,7 +36,7 @@ const menu = [
       title: "egg attack",
       category: "lunch",
       price: 22.99,
-      img: "./2.jpg",
+      img: "./img/2.jpg",
       desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
@@ -44,7 +44,7 @@ const menu = [
       title: "oreo dream",
       category: "shakes",
       price: 18.99,
-      img: "./3.jpg",
+      img: "./img/3.jpg",
       desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
 ];
@@ -52,18 +52,55 @@ const menu = [
 console.log(menu)
 
 const filterBtn = document.querySelectorAll(".btn-container button")
+const innerItem = document.querySelector('.section-center')
 
-filterBtn.forEach(function(btn) {
-    const category2 = btn.dataset.name;
-    btn.innerText = category2
-    btn.addEventListener("click", function(e){
-        const category = e.target.dataset.id;
-        const menuCategory = menu.filter(function(menuItem){
-            if(category === "all"){
-                return true;
-            }else if(menuItem.category === category){
-                return menuItem
-            }
-        })
-    })
-})
+
+
+const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+// load items
+window.addEventListener("DOMContentLoaded", function () {
+  // 페이지가 로드되었을 때 실행되는 이벤트 리스너
+  // displayMenuItems 함수를 호출하여 초기 메뉴 항목들을 표시
+  displayMenuItems(menu);
+});
+
+// filter items
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.target.dataset.name;
+    console.log(category)
+    const menuCategory = menu.filter(function (menuItem) {
+      if (category === "all") {
+        return true; // 모든 항목을 반환
+      } else if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+
+    displayMenuItems(menuCategory);//화면 표시
+  });
+});
+
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+      <img src=${item.img} alt=${item.title} class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">${item.price}</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>`;
+  });
+
+  displayMenu = displayMenu.join("");
+
+  sectionCenter.innerHTML = displayMenu;
+}
